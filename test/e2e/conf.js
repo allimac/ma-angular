5e334b7962a442df2dbffc80d15b68367e80ab18
allimac <camillacolser@hotmail.com>
2016-01-27 20:43:34 +0000

    added dependency on package.json

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -6,5 +6,17 @@ exports.config = {
   },
   jasmineNodeOpts: {
     showColors: true, // Use colors in the command line report.
+  },
+
+  onPrepare: function() {
+
+    var jasmineReporters = require('jasmine-reporters');
+
+    jasmine.getEnv().addReporter(new jasmineReporters.TerminalReporter({
+      verbosity: 10,
+      color: true,
+      showStack: true
+    }));
   }
+
 };
