64dc51c35f433a0bf93abd48a705aeec1efff077
allimac <camillacolser@hotmail.com>
2016-01-27 20:46:47 +0000

    token

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,6 +1,7 @@
 app.factory('Search', ['$http', function($http) {
 
   var queryUrl = 'https://api.github.com/search/users';
+  var secretissimo = 'd3ad2562df6612a257f67cefe97d77928492e58f';
 
   return {
     query: function(searchTerm) {
