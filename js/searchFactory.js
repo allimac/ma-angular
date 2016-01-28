c40c765ee2201fdfe91036750d550d758a995aac
allimac <camillacolser@hotmail.com>
2016-01-28 19:23:30 +0000

    failed attempt to get user repos

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,17 +1,32 @@
 app.factory('Search', ['$http', function($http) {
 
   var queryUrl = 'https://api.github.com/search/users';
+  var userData;
+
+  var addReposData = function(usersData) {
+    usersData.data.items.forEach(function(item) {
+      $http({
+        url: item.repos_url,
+        method: 'GET'
+      }).then(function(response) {
+        item.repos = response;
+      });
+    });
+  };
 
   return {
     query: function(searchTerm) {
-      return $http({
-        url: queryUrl,
-        method: 'GET',
-        params: {
-          access_token: token,
-          'q': searchTerm
+      $http({
+      url: queryUrl,
+      method: 'GET',
+      params: {
+        access_token: token,
+        'q': searchTerm
         }
-      });
+      }).then(function(response) {
+        return addReposData(response);
+      }
+      );
     }
   };
 }]);
--- test/searchfactory.spec.js
+++ test/searchfactory.spec.js
@@ -1,18 +1,39 @@
 describe('factory: Search', function() {
   var search;
   var httpBackend;
+  var repos = [
+    {
+      id: 7008074,
+      name: "accessory-samples",
+      full_name: "spike/accessory-samples",
+      html_url: "https://github.com/spike/accessory-samples",
+      description: "Talk to Your Toaster Accessory Sample Code",
+      language: "Java",
+    },
+    {
+      id: 345,
+      name: "another-repos",
+      full_name: "spike/another-repo",
+      html_url: "https://github.com/spike/another-repo",
+      description: "Talk to Your Toaster another repo Code",
+      language: "Spanish",
+    }
+  ];
   var items = [
     {
       "login": "tansaku",
       "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "http://github.com/tansaku"
+      "html_url": "http://github.com/tansaku",
+      "repos": repos
     },
     {
       "login": "stephenlloyd",
       "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
+      "html_url": "https://github.com/stephenlloyd",
+      "repos": repos
     }
   ];
+
 
   beforeEach(module('GitUserSearch'));
   beforeEach(inject(function(Search) {
@@ -20,9 +41,13 @@ describe('factory: Search', function() {
   }));
   beforeEach(inject(function($httpBackend) {
     httpBackend = $httpBackend;
-    httpBackend.when("GET", "https://api.github.com/search/users?access_token="+token+"&q=hello")
+    httpBackend.when("GET", "https://api.github.com/search/users?access_token="+token+"&q=tansaku")
                 .respond( {
                   items: items
+                });
+    httpBackend.when("GET", "https://api.github.com/users/tansaku/repos")
+                .respond( {
+                  repos: repos
                 });
   }));
 
@@ -33,10 +58,18 @@ describe('factory: Search', function() {
 
 
   it('returns search results', function() {
-    search.query('hello').then(function(response) {
+    search.query('tansaku').then(function(response) {
       expect(response.data.items).toEqual(items);
     });
     httpBackend.flush();
   });
+
+  it('returns repos data of all users combined with first source of data', function() {
+    search.full_data('tansaku').then(function(response) {
+      expect(response.data.items[0].repos).toEqual(repos);
+    });
+    httpBackend.flush();
+  });
+
 
 });
